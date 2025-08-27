2a8eb630dd1b3eec787d14af88bc6a68cf54385d
Dav1d22 <fierro.david2206@gmail.com>
2025-08-26 20:47:36 -0500

    v1_david

---
+++ ejercicio_B_C.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORMULARIO</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <input type="number" name="TABLA" placeholder="INGRESE TABLA" required> 
+        <br>
+        <br>
+        <button type="submit">GENERAR TABLA</button>
+    </form>
+
+
+</body>
+</html>
+<?php
+   if(isset($_POST["TABLA"])){
+
+        $numero=$_POST["TABLA"];
+
+        for($i =1; $i <10; $i++){
+            $resultado=$numero*$i;
+            echo $numero, "x", $i, "=", $resultado, "<br>" ; 
+            
+        }
+
+
+   };
+    
+     
+
+
+?>
